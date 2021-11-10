9541504f1334b75e42b7e8fdd2b9533ec70be90a
Ubuntu <ubuntu@ip-172-18-41-142.sa-east-1.compute.internal>
2021-11-10 17:57:20 +0000

    maquina 1

--- 01-terraform_lab_exemplos/02-resource_file_count/main.tf
+++ 01-terraform_lab_exemplos/02-resource_file_count/main.tf
@@ -1,5 +1,5 @@
 resource "local_file" "teste" {
-  count = 3
-  filename = "teste${count.index}.txt"
-  content = "Olá alunos bem vindo ao terraform ${count.index}.0"
+  count = 10
+  filename = "aula${count.index}.txt"
+  content = "Olá alunos bem vindo ao terraform ${count.index}"
 }
