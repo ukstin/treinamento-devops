2340b967952ffd80f18c81492e22e3db51b5673a
ukstin <ukstin@gmail.com>
2021-11-22 15:31:26 +0000

    Jenkins ajuste deploy.sh

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 cd 08-jenkins/deploy-infra-img-java-app/terraform
-/home/ubuntu/terraform init
-/home/ubuntu/terraform apply -auto-approve
+terraform init
+terraform apply -auto-approve
 
 echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
 echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo
 echo "$(/home/ubuntu/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+echo "[ec2-jenkins:vars]" >> ../1-ansible/hosts
+echo "ansible_python_interpreter=/usr/bin/python3" >> ../1-ansible/hosts
 
 echo "Aguardando criação de maquinas ..."
+
 sleep 10 # 10 segundos
 
 cd ../ansible
 
-echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa ]"
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa
+echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/privatekey_mysql_uk.pem --ssh-common-args='-o StrictHostKeyChecking=no']"
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/privatekey_mysql_uk.pem --ssh-common-args='-o StrictHostKeyChecking=no'
