044fbe0af3a253a2dc1e268f8fe39eb06a1a1090
ukstin <ukstin@gmail.com>
2021-11-22 15:39:45 +0000

    ajuste .sh

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -8,7 +8,7 @@ echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
 echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo
-echo "$(/home/ubuntu/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
 echo "[ec2-jenkins:vars]" >> ../1-ansible/hosts
 echo "ansible_python_interpreter=/usr/bin/python3" >> ../1-ansible/hosts
 
