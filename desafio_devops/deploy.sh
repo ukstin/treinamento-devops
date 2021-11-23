85fd2bc5bda37465064073f7d18ebd635ebae575
ukstin <ukstin@gmail.com>
2021-11-23 17:55:18 +0000

    ajuste shell

--- desafio_devops/deploy.sh
+++ desafio_devops/deploy.sh
@@ -6,8 +6,8 @@ terraform init
 #terraform import aws_internet_gateway.igw_desafio igw-00383a5bb2468a105
 terraform apply -auto-approve
 
-echo "Aguardando criação de maquinas ..."
-sleep 10 # 10 segundos
+echo "Rede criada com sucesso ..."
+#sleep 10 # 10 segundos
 
 # echo $"[ec2-image]" > ../ansible/hosts # cria arquivo
 # echo "$(terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" > hosts # captura output faz split de espaco e replace de ",
--- desafio_devops/destroy.sh
+++ desafio_devops/destroy.sh
@@ -4,10 +4,10 @@ cd desafio_devops
 #terraform init
 #terraform import aws_vpc.vpc_desafio vpc-0b7bc0aae8788da62
 #terraform import aws_internet_gateway.igw_desafio igw-00383a5bb2468a105
-terraform destroy
+terraform destroy -auto-approve
 
-echo "Aguardando criação de maquinas ..."
-sleep 10 # 10 segundos
+echo "Rede destruida ..."
+#sleep 10 # 10 segundos
 
 # echo $"[ec2-image]" > ../ansible/hosts # cria arquivo
 # echo "$(terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" > hosts # captura output faz split de espaco e replace de ",
