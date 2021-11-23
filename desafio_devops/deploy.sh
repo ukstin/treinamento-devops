11ea80764a70562a19dbf71fdb1b765af94d344d
ukstin <ukstin@gmail.com>
2021-11-23 16:54:28 +0000

    Pipeline 1 - network

---
+++ desafio_devops/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd desafio_devops
+terraform init
+terraform import aws_vpc.vpc_desafio vpc-0b7bc0aae8788da62
+terraform import aws_internet_gateway.igw_desafio igw-00383a5bb2468a105
+terraform apply -auto-approve
+
+echo "Aguardando criação de maquinas ..."
+sleep 10 # 10 segundos
+
+# echo $"[ec2-image]" > ../ansible/hosts # cria arquivo
+# echo "$(terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" > hosts # captura output faz split de espaco e replace de ",
+# echo "[ec2-image:vars]" >> hosts
+# echo "ansible_python_interpreter=/usr/bin/python3" >> hosts
+
+# echo "Aguardando criação de maquinas ..."
+
+# sleep 10 # 10 segundos
+
+# echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/privatekey_mysql_uk.pem --ssh-common-args='-o StrictHostKeyChecking=no']"
+# ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/privatekey_mysql_uk.pem --ssh-common-args='-o StrictHostKeyChecking=no'
--- desafio_devops/main.tf
+++ desafio_devops/main.tf
@@ -4,99 +4,99 @@ provider "aws" {
 
 #terraform import aws_vpc.vpc_uk vpc-0b7bc0aae8788da62
 # vpc-id = vpc-0b7bc0aae8788da62
-resource "aws_vpc" "vpc_uk" {
-    cidr_block = "172.18.0.0/16"
-    tags = {
-        Name = "vpc_uk"
-    }
-}
+// resource "aws_vpc" "vpc_desafio" {
+//     cidr_block = "172.18.0.0/16"
+//     tags = {
+//         Name = "vpc_desafio"
+//     }
+// }
 
 # terraform import aws_internet_gateway.igw igw-<id>
 # igw ig = igw-00383a5bb2468a105
-resource "aws_internet_gateway" "igw_uk" {
-    vpc_id = aws_vpc.vpc_uk.id
+resource "aws_internet_gateway" "igw_desafio" {
+    vpc_id = aws_vpc.vpc_desafio
     tags = {
-        Name = "internet-gateway-uk"
+        Name = "internet-gateway-desafio"
     }
 }
 
 resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
-    vpc_id = aws_vpc.vpc_uk.id
-    cidr_block = "172.19.0.0/16"
+    vpc_id = aws_vpc.vpc_desafio.id
+    cidr_block = "172.20.0.0/16"
 }
 
-resource "aws_subnet" "subnet_uk_azA_public" {
+resource "aws_subnet" "subnet_desafio_azA_public" {
   vpc_id            = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
-  cidr_block        = "172.19.1.0/24"
+  cidr_block        = "172.20.1.0/24"
   availability_zone = "sa-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "subnet_uk_azA_tf_public"
+    Name = "subnet_desafio_azA_public"
   }
 }
 
-resource "aws_subnet" "subnet_uk_azB_public" {
+resource "aws_subnet" "subnet_desafio_azB_public" {
   vpc_id            = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
-  cidr_block        = "172.19.2.0/24"
+  cidr_block        = "172.20.2.0/24"
   availability_zone = "sa-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "subnet_uk_azB_tf_public"
+    Name = "subnet_desafio_azB_public"
   }
 }
 
-resource "aws_subnet" "subnet_uk_azC_public" {
+resource "aws_subnet" "subnet_desafio_azC_public" {
   vpc_id            = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
-  cidr_block        = "172.19.3.0/24"
+  cidr_block        = "172.20.3.0/24"
   availability_zone = "sa-east-1c"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "subnet_uk_azC_tf_public"
+    Name = "subnet_desafio_azC_public"
   }
 }
 
-resource "aws_subnet" "subnet_uk_azA_private" {
+resource "aws_subnet" "subnet_desafio_azA_private" {
   vpc_id            = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
-  cidr_block        = "172.19.4.0/24"
+  cidr_block        = "172.20.4.0/24"
   availability_zone = "sa-east-1a"
 
   tags = {
-    Name = "subnet_uk_azA_tf_private"
+    Name = "subnet_desafio_azA_private"
   }
 }
 
-resource "aws_subnet" "subnet_uk_azB_private" {
+resource "aws_subnet" "subnet_desafio_azB_private" {
   vpc_id            = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
-  cidr_block        = "172.19.5.0/24"
+  cidr_block        = "172.20.5.0/24"
   availability_zone = "sa-east-1b"
 
   tags = {
-    Name = "subnet_uk_azB_tf_private"
+    Name = "subnet_desafio_azB_private"
   }
 }
 
-resource "aws_subnet" "subnet_uk_azC_private" {
+resource "aws_subnet" "subnet_desafio_azC_private" {
   vpc_id            = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
-  cidr_block        = "172.19.6.0/24"
+  cidr_block        = "172.20.6.0/24"
   availability_zone = "sa-east-1c"
 
   tags = {
-    Name = "subnet_uk_azC_tf_private"
+    Name = "subnet_desafio_azC_private"
   }
 }
 
-resource "aws_route_table" "rt_uk_private" {
+resource "aws_route_table" "rt_desafio_private" {
   vpc_id = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
   route = []
   tags = {
-    Name = "route_table_uk_private"
+    Name = "route_table_desafio_private"
   }
 }
 
-resource "aws_route_table" "rt_uk_public" {
+resource "aws_route_table" "rt_desafio_public" {
   vpc_id = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
   route = [
       {
@@ -104,7 +104,7 @@ resource "aws_route_table" "rt_uk_public" {
         cidr_block                 = "0.0.0.0/0"
         destination_prefix_list_id = ""
         egress_only_gateway_id     = ""
-        gateway_id                 = aws_internet_gateway.igw_uk.id
+        gateway_id                 = aws_internet_gateway.igw_desafio.id
         instance_id                = ""
        ipv6_cidr_block            = ""
         local_gateway_id           = ""
@@ -117,59 +117,43 @@ resource "aws_route_table" "rt_uk_public" {
   ]
 
   tags = {
-    Name = "route_table_uk_public"
+    Name = "route_table_desafio_public"
   }
 }
 
 resource "aws_route_table_association" "subnet1_public" {
-  subnet_id      = aws_subnet.subnet_uk_azA_public.id
-  route_table_id = aws_route_table.rt_uk_public.id
+  subnet_id      = aws_subnet.subnet_desafio_azA_public.id
+  route_table_id = aws_route_table.rt_desafio_public.id
 }
 
 resource "aws_route_table_association" "subnet2_public" {
-    subnet_id      = aws_subnet.subnet_uk_azB_public.id
-    route_table_id = aws_route_table.rt_uk_public.id
+    subnet_id      = aws_subnet.subnet_desafio_azB_public.id
+    route_table_id = aws_route_table.rt_desafio_public.id
 }
 
 resource "aws_route_table_association" "subnet3_public" {
-    subnet_id      = aws_subnet.subnet_uk_azC_public.id
-    route_table_id = aws_route_table.rt_uk_public.id
+    subnet_id      = aws_subnet.subnet_desafio_azC_public.id
+    route_table_id = aws_route_table.rt_desafio_public.id
 }
 
 resource "aws_route_table_association" "subnet4_private" {
-  subnet_id      = aws_subnet.subnet_uk_azA_private.id
-  route_table_id = aws_route_table.rt_uk_private.id
+  subnet_id      = aws_subnet.subnet_desafio_azA_private.id
+  route_table_id = aws_route_table.rt_desafio_private.id
 }
 
 resource "aws_route_table_association" "subnet5_private" {
-    subnet_id      = aws_subnet.subnet_uk_azB_private.id
-    route_table_id = aws_route_table.rt_uk_private.id
+    subnet_id      = aws_subnet.subnet_desafio_azB_private.id
+    route_table_id = aws_route_table.rt_desafio_private.id
 }
 
 resource "aws_route_table_association" "subnet6_private" {
-    subnet_id      = aws_subnet.subnet_uk_azC_private.id
-    route_table_id = aws_route_table.rt_uk_private.id
+    subnet_id      = aws_subnet.subnet_desafio_azC_private.id
+    route_table_id = aws_route_table.rt_desafio_private.id
 }
 
-// provider "aws" {
-//   region = "sa-east-1"
-// }
-
-resource "aws_instance" "web_uk" {
-  ami                     = data.aws_ami.ubuntu.id
-  instance_type           = "t3.micro"
-  key_name                = "privatekey_mysql_uk" # key chave publica cadastrada na AWS 
-  subnet_id               =  aws_subnet.subnet_uk_azB_public.id # vincula a subnet direto e gera o IP automático
-  #private_ip              = "172.17.0.100"
-  vpc_security_group_ids  = [
-    "${aws_security_group.allow_ssh_terraform.id}",
-  ]
-  root_block_device {
-    encrypted = true
-    volume_size = 8
-  }
-
-  tags = {
-    Name = "ec2-uk-tf-nginx"
-  }
-}
+// output "subnet_output" {
+//   value = [
+//     aws_instance.web.private_ip,
+//     "ssh -i ~/Desktop/devops/treinamentoItau ubuntu@${aws_instance.web.private_dns}"
+//   ]
+//}
